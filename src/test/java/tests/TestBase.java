cc461cb4ee1025d70d248c9a3f315d7e022fff0f
matuation <git.user>
2026-01-27 22:08:55 +0300

    removed commented code

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -24,11 +24,8 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        //Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.pageLoadStrategy = "eager";
-        //Configuration.holdBrowserOpen = true;
-        //Configuration.timeout = 5000; // default 4000
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
