35696629620149358651b07f82174e3327828641
matuation <git.user>
2026-01-31 20:46:38 +0300

    1. added allure-notifications-4.11.0.jar 2. Updated BuildParameters.java for local launches

--- src/test/java/config/BuildParameters.java
+++ src/test/java/config/BuildParameters.java
@@ -6,7 +6,7 @@ public class BuildParameters {
     public String baseUrl = System.getProperty("baseUrl", "https://demoqa.com/");
     public String remoteBaseUsername = System.getProperty("remoteBaseUsername");
     public String remoteBasePass = System.getProperty("remoteBasePass");
-    public String remoteBaseUrl = System.getProperty("remoteBaseUrl", "selenoid.autotests.cloud/wd/hub");
+    public String remoteBaseUrl = System.getProperty("remoteBaseUrl");
     public String isHeadless = System.getProperty("isHeadless", "false");
     public String browserSize = System.getProperty("browserSize", "1920x1080");
     public String pageLoadStrat = System.getProperty("pageLoadStrat", "eager");
